d6114a404b31c2371d6d75f5f6c2cedd6633788f
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-09-29 11:26:09 +0530

    Update solution_clean_city.py

--- solution_clean_city.py
+++ solution_clean_city.py
@@ -8,21 +8,26 @@ width=600
 height=350
 screen = pygame.display.set_mode((width,height))
   
-#load the images in dict
+# Loading the images in 'images' dictionary
 images={}
 images["bg"] = pygame.image.load("city3.jpg").convert_alpha()
 images["bin"] = pygame.image.load("bin.png").convert_alpha()
   
 
 class Dustbin:
+    # Defining the '__init__()' function which takes 'self' and 'x' as arguments
+    # 'x' represents the x-coordinate of the bins which will be passed while creating the objects
     def __init__(self,x):
         self.bin=pygame.Rect(x,210,10,10)
         
     def display(self):
         screen.blit(images["bin"],self.bin)
-        
+
+# Creating 'bin1'        
 bin1=Dustbin(25)
+# Creating 'bin2'  
 bin2=Dustbin(335)
+# Creating 'bin3'  
 bin3=Dustbin(516)
 
 while True:    
@@ -39,8 +44,11 @@ while True:
     
     
     screen.blit(images["bg"],[0,0])
+    # Calling the 'display' function for 'bin1' object
     bin1.display()
+    # Calling the 'display' function for 'bin2' object
     bin2.display()
+    # Calling the 'display' function for 'bin3' object
     bin3.display()
     
 
